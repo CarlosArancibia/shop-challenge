e697c253cf5cf925f09bdabaf1029b0767469bae
CarlosArancibia <arancibiacarlos.b@gmail.com>
2025-07-02 22:34:13 -0400

    shop-app: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='bg-slate-800 min-h-screen text-gray-100'>
+      <div className='max-w-[900px] m-auto px-3'>
+        <h1>React Shop</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
